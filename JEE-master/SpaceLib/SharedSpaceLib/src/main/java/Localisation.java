bb928ee79b1cb63f0ff51f189b4034c053bd275e
mahachaouch <maha-chaouch@hotmail.fr>
2018-05-31 10:49:49 +0200

    réparrer le shared

---
+++ JEE-master/SpaceLib/SharedSpaceLib/src/main/java/Localisation.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author yannl
+ */
+public class Localisation {
+    private float x;
+    private float y;
+
+    public Localisation(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+    
+}
