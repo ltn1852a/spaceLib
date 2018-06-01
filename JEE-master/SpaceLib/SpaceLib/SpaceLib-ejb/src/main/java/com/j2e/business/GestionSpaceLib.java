bf68da2cb561a47d69714c7803a4604d75573021
mahachaouch <maha-chaouch@hotmail.fr>
2018-06-01 12:39:13 +0200

    last commit to start fresh :p

--- JEE-master/SpaceLib/SpaceLib/SpaceLib-ejb/src/main/java/com/j2e/business/GestionSpaceLib.java
+++ JEE-master/SpaceLib/SpaceLib/SpaceLib-ejb/src/main/java/com/j2e/business/GestionSpaceLib.java
@@ -6,7 +6,9 @@
 package com.j2e.business;
 
 import com.j2e.exceptions.StationNotFoundException;
+import com.j2e.repositories.StationFacade;
 import java.util.Map;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 /**
@@ -16,9 +18,12 @@ import javax.ejb.Stateless;
 @Stateless
 public class GestionSpaceLib implements GestionSpaceLibRemote {
 
+    @EJB
+    private StationFacade statF;
+    
     @Override
     public void créerStation(Map<Integer, Integer> quais, Localisation loc) {
-        
+        statF.créerStation(quais, loc);
     }
 
     @Override
@@ -30,6 +35,4 @@ public class GestionSpaceLib implements GestionSpaceLibRemote {
     public void créerQuai(int idStation) throws StationNotFoundException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
- 
 }
