4cf41083f36de76e666206d3cfb160a022fb17a3
Yann Lattes <yann.lattes@hotmail.fr>
2018-06-07 13:32:03 +0200

    SpaceLibAdmin initiliased but not finished

--- JEE-master/SpaceLib/SharedSpaceLib/src/main/java/com/j2e/exceptions/userAlreadyExistsException.java
+++ JEE-master/SpaceLib/SharedSpaceLib/src/main/java/com/j2e/exceptions/userAlreadyExistsException.java
@@ -2,9 +2,8 @@ package com.j2e.exceptions;
 
 
 public class userAlreadyExistsException extends Exception{
-
-	public userAlreadyExistsException(){
-		super();
-	}
+    public userAlreadyExistsException(String alreadyExist) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
