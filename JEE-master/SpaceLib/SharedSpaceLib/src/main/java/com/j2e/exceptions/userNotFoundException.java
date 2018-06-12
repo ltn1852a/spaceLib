afc5334533624ba54839c76d7dad585482820862
Yann Lattes <yann.lattes@hotmail.fr>
2018-06-12 17:15:57 +0200

    INSERT SQL

--- JEE-master/SpaceLib/SharedSpaceLib/src/main/java/com/j2e/exceptions/userNotFoundException.java
+++ JEE-master/SpaceLib/SharedSpaceLib/src/main/java/com/j2e/exceptions/userNotFoundException.java
@@ -4,7 +4,7 @@ package com.j2e.exceptions;
 public class userNotFoundException extends Exception{
 
 	public userNotFoundException(){
-		super();
+		super("Utilisateur non connu");
 	}
 	public userNotFoundException(String msg){
 		super(msg);
